072e7cbe248dac5e8b74b4fd2b9a4ee887ae7bd6
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-07-12 22:14:22 +0530

    Update vernos_public_pack.py

--- bin/commands/vernos_public_pack.py
+++ bin/commands/vernos_public_pack.py
@@ -1,2 +1,14 @@
+import sys
+import os
+import subprocess
+
 def vernos_public_ver():
+     print("vernos version : 0.1")
+     print("pack version : 1")
+ 
+def vernos_token_c103115a():
+     print("Token : Bx4asa35g9")
+     
+
+
      
