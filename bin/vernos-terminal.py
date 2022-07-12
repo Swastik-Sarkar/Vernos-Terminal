80ec3e9aad6714e2771d0573a786945de450e66b
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-07-12 18:28:21 +0530

    Update vernos-terminal.py

--- bin/vernos-terminal.py
+++ bin/vernos-terminal.py
@@ -5,3 +5,12 @@ import json
 import random
 import time
 
+path = 'C:/'
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print("Vernos Terminal v0.1 Stable ")
+while True:
+	code = input("$ ")
+	if code == 'ip':
+		print("IP: " + host_ip)
+		print("Machine Name is " + host_name)
