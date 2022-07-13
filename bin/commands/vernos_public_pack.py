b0af2eddeb928acd237346d6d5bd14a75b108fde
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-07-13 20:09:31 +0530

    Update vernos_public_pack.py

--- bin/commands/vernos_public_pack.py
+++ bin/commands/vernos_public_pack.py
@@ -33,39 +33,29 @@ def vernos_command_rsh():
       "Make sure you are typing the data correctly. There's no error-catching as of yet." + "\n"
                                                                                             "Let's start!")
 
-hostname = input("What's the name for the router?")
-if hostname == "":
-  hostname = "Router"
+     hostname = input("What's the name for the router?")
+     if hostname == "":
+     hostname = "Router"
 
-numberOfSubnets = int(input("How many local subnetworks are connected to the router?"))
+     numberOfSubnets = int(input("How many local subnetworks are connected to the router?"))
 
-# TODO: Make the numberOfSubnets input idiot-proof.
-# Find a way to TRY to convert float into int, if not possible, throw an error.
+     while numberOfSubnets <= 0:
+     numberofSubnets = int(input("Type a correct number of local subnetworks."))
+     print("Number of subnets is " + str(numberOfSubnets))
 
-while numberOfSubnets <= 0:
-  numberofSubnets = int(input("Type a correct number of local subnetworks."))
-print("Number of subnets is " + str(numberOfSubnets))
+     interfacesDictionary = {}
+     for i in range(numberOfSubnets):
+       interfaceIP = input("Type IP address of your " + str(i + 1) + " subnet: ")
+       interfaceName = input("Type what interface will the subnet be assigned to: ")
+       interfacesDictionary[interfaceName] = interfaceIP
+       print("The IP address of your " + str(i + 1) + " subnet is: " + interfaceIP)
+       print("The assigned interface: " + interfaceName)
 
-# Change list to the dictionary, let the key name be a name of the port
-interfacesDictionary = {}
-for i in range(numberOfSubnets):
-  interfaceIP = input("Type IP address of your " + str(i + 1) + " subnet: ")
-  interfaceName = input("Type what interface will the subnet be assigned to: ")
-  interfacesDictionary[interfaceName] = interfaceIP
-  print("The IP address of your " + str(i + 1) + " subnet is: " + interfaceIP)
-  print("The assigned interface: " + interfaceName)
+     defaultGateway = str(input(
+      "Do you want the router to have default ip as .254 in the subnets or you prefer custom ones? Type either "
+      "'default' or 'custom'."))
 
-# For future use, the number of end devices might be useful when you decide if your subnet is too small or too big.
-# For now, scrap that up.
-'''
-for key, value in interfacesDictionary.items():
-  numberOfEndDevices = input("How many end devices will be connected to the subnet " + str(value) + "?")
-'''
-defaultGateway = str(input(
-  "Do you want the router to have default ip as .254 in the subnets or you prefer custom ones? Type either "
-  "'default' or 'custom'."))
-# Fix the logic statement here.
-while defaultGateway != "default" or defaultGateway != "custom":
-  defaultGateway = input("Wrong answer! Type either 'default' or 'custom'!")
-print(defaultGateway)
+     while defaultGateway != "default" or defaultGateway != "custom":
+        defaultGateway = input("Wrong answer! Type either 'default' or 'custom'!")
+     print(defaultGateway)
      
