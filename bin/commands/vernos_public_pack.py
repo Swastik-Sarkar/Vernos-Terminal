4b3688fb5cb01340857c380b2b26532961743ad5
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-07-14 17:52:08 +0530

    Update vernos_public_pack.py

--- bin/commands/vernos_public_pack.py
+++ bin/commands/vernos_public_pack.py
@@ -26,36 +26,3 @@ def vernos_public_details():
 
 #commands 
 #command.router_setup_helper
-def vernos_command_rsh():
-     print("Configuring a single router on Cisco Packet Tracer. Simplified." + "\n"
-      + "Basically answer all the questions and the program will prepare a script you have to type in the router to "
-        "get desired configuration." + "\n" +
-      "Make sure you are typing the data correctly. There's no error-catching as of yet." + "\n"
-                                                                                            "Let's start!")
-
-     hostname = input("What's the name for the router?")
-     if hostname == "":
-     hostname = "Router"
-
-     numberOfSubnets = int(input("How many local subnetworks are connected to the router?"))
-
-     while numberOfSubnets <= 0:
-     numberofSubnets = int(input("Type a correct number of local subnetworks."))
-     print("Number of subnets is " + str(numberOfSubnets))
-
-     interfacesDictionary = {}
-     for i in range(numberOfSubnets):
-       interfaceIP = input("Type IP address of your " + str(i + 1) + " subnet: ")
-       interfaceName = input("Type what interface will the subnet be assigned to: ")
-       interfacesDictionary[interfaceName] = interfaceIP
-       print("The IP address of your " + str(i + 1) + " subnet is: " + interfaceIP)
-       print("The assigned interface: " + interfaceName)
-
-     defaultGateway = str(input(
-      "Do you want the router to have default ip as .254 in the subnets or you prefer custom ones? Type either "
-      "'default' or 'custom'."))
-
-     while defaultGateway != "default" or defaultGateway != "custom":
-        defaultGateway = input("Wrong answer! Type either 'default' or 'custom'!")
-     print(defaultGateway)
-     
