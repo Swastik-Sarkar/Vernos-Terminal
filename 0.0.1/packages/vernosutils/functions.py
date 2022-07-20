89f2f42aade19f812ab533017a509358bc87b7e3
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-07-20 20:39:20 +0530

    Update functions.py

--- 0.0.1/packages/vernosutils/functions.py
+++ 0.0.1/packages/vernosutils/functions.py
@@ -2,5 +2,11 @@ import sys
 import os
 import random
 import time
+from coloroma import *
 
 version="0.0.1"
+
+def utilver():
+  
+  @util.version
+  print(Fore.CYAN + "Verssion : 0.0.1" + Fore.WHITE)
